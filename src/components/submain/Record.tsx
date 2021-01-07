70160655dbc6fe40d8beb783019320fc5587b060
kayacchang <egg734631@gmail.com>
2021-01-07 23:34:01 +0800

    add 3 kind of record

--- src/components/submain/Record.tsx
+++ src/components/submain/Record.tsx
@@ -6,34 +6,74 @@ interface Props extends Avator, Transaction {
   date: string;
 }
 
-export default function Record({
-  description,
-  amount,
-  balance,
-  img,
-  name,
-  date,
-  className,
-}: Props) {
+function Type1({ description, amount, balance, date, name }: Props) {
+  return (
+    <div className="flex-1 flex text-sm">
+      <header className="w-2/6 flex flex-col justify-center">
+        <h3>{name}</h3>
+
+        <span className="text-xs">{date}</span>
+      </header>
+
+      <section className="flex-1 flex items-center">
+        <p>{description}</p>
+      </section>
+
+      <section className="flex flex-col justify-center text-right">
+        <h2 className="text-2xl">{amount}</h2>
+
+        <span className="text-xs">{balance}</span>
+      </section>
+    </div>
+  );
+}
+
+function Type2({ description, amount, balance, date, name }: Props) {
+  return (
+    <div className="flex-1 flex text-sm">
+      <section className="flex-1 flex flex-col justify-center">
+        <span className="text-xs">
+          {date} - {name}
+        </span>
+
+        <p>{description}</p>
+      </section>
+
+      <section className="flex flex-col justify-center text-right">
+        <h2 className="text-2xl">{amount}</h2>
+
+        <span className="text-xs">{balance}</span>
+      </section>
+    </div>
+  );
+}
+
+function Type3({ description, amount, balance, date, name }: Props) {
+  return (
+    <div className="flex-1 flex text-sm">
+      <section className="flex-1 flex flex-col justify-center">
+        <p>{description}</p>
+
+        <span className="text-xs">
+          {date} - {name}
+        </span>
+      </section>
+
+      <section className="flex flex-col justify-center text-right">
+        <h2 className="text-2xl">{amount}</h2>
+
+        <span className="text-xs">{balance}</span>
+      </section>
+    </div>
+  );
+}
+
+export default function Record(props: Props) {
+  const { name, img, className } = props;
+
   return (
     <AvatarGroup name={name} img={img} className={className}>
-      <div className="flex-1 flex text-sm">
-        <header className="w-2/6 flex flex-col justify-center">
-          <h3>{name}</h3>
-
-          <span className="text-xs">{date}</span>
-        </header>
-
-        <section className="flex-1 flex items-center">
-          <p>{description}</p>
-        </section>
-
-        <section className="flex flex-col justify-center text-right">
-          <h2 className="text-2xl">{amount}</h2>
-
-          <span className="text-xs">{balance}</span>
-        </section>
-      </div>
+      <Type3 {...props} />
     </AvatarGroup>
   );
 }
