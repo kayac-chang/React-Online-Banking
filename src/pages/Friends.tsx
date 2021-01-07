f02f2066de66ce18b5874d689f43f1508c826234
kayacchang <egg734631@gmail.com>
2021-01-07 16:32:59 +0800

    refactor friends

--- src/pages/Friends.tsx
+++ src/pages/Friends.tsx
@@ -1,29 +1,31 @@
+import { fillRemainHeight } from "utils";
+import { Avator } from "types";
+import { map } from "ramda";
+
 import Avatar from "components/base/atoms/Avatar";
 import SearchField from "components/base/molecules/SearchField";
 import Header from "components/base/organisms/Header";
-import { fillRemainHeight } from "utils";
 
 import friends from "mocks/friends";
+
+const toAvatar = ({ img, name }: Avator) => (
+  <Avatar key={name} img={img} name={name} showName />
+);
 
 export default function Friends() {
   return (
     <>
       <Header title="好友轉帳" subTitle="點選好友快速轉帳" align="start" />
 
-      <main className="px-4">
+      <div className="mx-4">
         <SearchField />
+      </div>
 
-        <article>
-          <div
-            className="grid grid-cols-3 gap-x-2 gap-y-8 p-4 overflow-y-auto"
-            ref={fillRemainHeight}
-          >
-            {friends.map(({ img, name }, index) => (
-              <Avatar key={index} img={img} name={name} showName />
-            ))}
-          </div>
-        </article>
-      </main>
+      <article className="py-4 overflow-y-auto" ref={fillRemainHeight}>
+        <div className="grid grid-cols-3 gap-x-6 gap-y-8 mx-8">
+          {map(toAvatar)(friends)}
+        </div>
+      </article>
     </>
   );
 }
