56d74fd844a6edddcfa8de339043db510bfba1d2
kayacchang <egg734631@gmail.com>
2021-02-12 22:18:27 +0800

    refactor submain

--- src/pages/SubMain/Root.tsx
+++ src/pages/SubMain/Root.tsx
@@ -6,9 +6,43 @@ import { BsSearch } from "react-icons/bs";
 
 import { Modal, Info } from "components/atoms";
 import { AvatarGroup, InputFieldWithPlaceholder } from "components/molecules";
-import { Header, BottomDrawer } from "components/organisms";
+import { Header } from "components/organisms";
 
 import transactions from "mocks/transactions";
+
+type Props = {
+  onClose?: () => void;
+};
+function BottomDrawer({ onClose }: Props) {
+  return (
+    <article className="absolute bottom-0 w-full h-1/3 bg-white flex flex-col">
+      <header className="mx-8 h-1/5">
+        <h3 className="h-full flex items-center text-lg">自訂區間</h3>
+      </header>
+
+      <section className="mx-8 flex-1 space-y-2">
+        <InputFieldWithPlaceholder
+          type="text"
+          label="起始日"
+          placeholder="請選擇"
+        />
+
+        <InputFieldWithPlaceholder
+          type="text"
+          label="結束日"
+          placeholder="請選擇"
+        />
+      </section>
+
+      <footer className="flex h-1/5 divide-x divide-white">
+        <button className="bg-button flex-1" onClick={onClose}>
+          取消
+        </button>
+        <button className="bg-button flex-1">查詢</button>
+      </footer>
+    </article>
+  );
+}
 
 type AccountProps = {
   balance: string;
